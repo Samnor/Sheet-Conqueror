5c633dac1e7bdbe0cb92cc2eaf4d6bec6f5dde34
Samnor <norling.samuel@gmail.com>
2016-05-08 16:01:29 +0200

    Created first cmdlet
    Read-ExcelVBAComponents does exactly what the name implies.
    //Samuel

---
+++ Read-ExcelVBAComponents.ps1
@@ -0,0 +1,48 @@
+﻿
+function Read-ExcelVBAComponents(){
+    <#
+    .SYNOPSIS
+    Extract all VBA components from an excel workbook.
+
+    .DESCRIPTION
+    Uses commands from PSExcel to extract VBA components from an excel workbook.
+
+    .PARAMETER workbookPath
+    The full path to the excel workbook that you wish to use.
+
+    .EXAMPLE
+    Coming soon
+
+    .INPUTS
+    Path strings only.
+
+    .OUTPUTS
+    Returns an ArrayList with VBA modules found. (System.Collections.ArrayList)
+
+    .NOTES
+    Dependencies: PSExcel 
+    Started writing this function 8th may 2016.
+
+    #>
+    [cmdletbinding()]
+    Param(
+        [Parameter(Mandatory=$true)]$workbookPath
+    )
+    if(!(Test-Path $workbookPath)){
+        Write-Verbose "workbookPath not found."
+    }
+    Get-Command -Module PSExcel | Out-Null
+    $returnArray = [System.Collections.ArrayList]@()
+    $excelObj = New-Excel -Path $workbookPath
+    $excelObj.Workbook.VbaProject.Modules | ForEach-Object {
+        $returnArray.Add($_)
+    }
+    $returnArray
+}
+
+
+$testPath = "C:\Users\Samuel\Desktop\CURRENT\Posh VBA\Test Dir\Equity Research\Models\Ericsson.xlsm"
+
+$readComponents = Read-ExcelVBAComponents $testPath
+$readComponents | % { $_.Code} 
+
