a618198c914947fdc9493c2692ea0e8a5bb3cf72
Samnor <norling.samuel@gmail.com>
2016-05-09 00:17:50 +0200

    New fixes to pesterfile

--- Tests/appveyor.pester.ps1
+++ Tests/appveyor.pester.ps1
@@ -1,14 +1,11 @@
 Import-Module $PSScriptRoot\..\Read-ExcelVBAComponents.psm1 -Force
 
-$correctPath = "$PSScriptRoot\..\Test Dir\Equity Research\Models\Ericsson.xlsm"
-
-$readComponents = Read-ExcelVBAComponents $testPath
-$readComponents | % { $_.Code}
-
-
 Describe 'Read-ExcelVBAComponents' {
     Context 'Strict mode' {
         
+        It "should throw an error when the path is empty" {
+            {Read-ExcelVBAComponents ""} | Should Throw
+        }
         It "should throw an error when the path is random letters" {
             {Read-ExcelVBAComponents "asC:\dgfdfgfd"} | Should Throw
         }
