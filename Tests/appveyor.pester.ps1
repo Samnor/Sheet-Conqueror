57daa2b90256aef5d6794ee2e07b5a0a70623b13
Samnor <norling.samuel@gmail.com>
2016-05-08 16:37:54 +0200

    Added first pester test
    Test if true is true
    //Sam

---
+++ Tests/appveyor.pester.ps1
@@ -0,0 +1,11 @@
+Import-Module -Force $PSScriptRoot\..\Read-ExcelVBAComponents.ps1
+
+Describe 'Read-ExcelVBAComponents' {
+    Context 'Strict mode' {
+        
+        It 'should be true when true' {
+            $true | Should be $true
+        }
+
+    }
+}
