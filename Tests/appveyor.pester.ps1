cdf257fd18cf681cf3703d04780f7cfad8b1fbbf
Samnor <norling.samuel@gmail.com>
2016-05-09 00:08:48 +0200

    fixed error argument in tests

--- Tests/appveyor.pester.ps1
+++ Tests/appveyor.pester.ps1
@@ -9,15 +9,14 @@ $readComponents | % { $_.Code}
 Describe 'Read-ExcelVBAComponents' {
     Context 'Strict mode' {
         
-        It "should throw an error when the path is empty" {
-            {Read-ExcelVBAComponents ""} | Should Throw
-        }
         It "should throw an error when the path is random letters" {
             {Read-ExcelVBAComponents "asC:\dgfdfgfd"} | Should Throw
         }
         It "should not throw an error when the path is valid" {
             {Read-ExcelVBAComponents $correctPath} | Should Not Throw
         }
+
+
 
     }
 }
